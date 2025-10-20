4a531dafe9c1a906f896b45f4e312c07364b3b86
Noor2-1 <mahnoorbutt214@gmail.com>
2025-10-20 11:10:44 +0500

    Splash Screen Done👍

---
+++ app/src/main/java/com/example/bookwormconnect/splashscreen.java
@@ -0,0 +1,36 @@
+package com.example.bookwormconnect;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashscreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+
+      VideoView valVideo=findViewById(R.id.Splash);
+      Uri videopath=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash_screen);
+        valVideo.setVideoURI(videopath);
+
+        valVideo.setOnCompletionListener(mp -> {
+            Intent intent=new Intent(splashscreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        valVideo.start();
+
+    }
+}
